b472626bb2cd455ff35b04b53990d6d3fa283ace
dongdada29 <dongdada29@gmail.com>
2025-12-04 15:13:26 +0800

    chore: 添加 react-vite 项目

---
+++ examples/react-vite/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import appdevDesignMode from '@xagi/vite-plugin-design-mode';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),
+      appdevDesignMode()
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  }
+});
